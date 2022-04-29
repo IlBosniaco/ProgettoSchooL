d4b18271da4072571cee582fe16a7926f5971584
SimmyZs <82151175+SimmyZs@users.noreply.github.com>
2022-04-29 18:03:46 +0200

    css profilo e registrazione

--- Sitoweb/Registrati/register.php
+++ Sitoweb/Registrati/register.php
@@ -10,55 +10,59 @@ require_once '../Database/config.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="functions.js"></script>
     <title>JMripetizioni - Registrati</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
 </head>
 
 <body>
-    <h2>Registrati per poter accedere al sito web</h2>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <p>Nome:
-            <input type="text" name="nome" id="" required autofocus>
-        </p>
-        <p>Cognome:
-            <input type="text" name="cognome" id="" required>
-        </p>
+    <div class="main-block">
+      <div class="left-part">
         
-        <p>Sesso:
-        <input type="radio" name="sesso" id="sesso" value="M">Maschio
-        <input type="radio" name="sesso"id="sesso" value="F">Femmina
-        
-        
-        <p>Classe: </p>
-        <p>
-            Anno:
-            <select name="anno" id="" required>
-                <option disabled selected value> -- Seleziona un'opzione -- </option>
+      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="title">
+          <i class="fas fa-pencil-alt"></i> 
+          <h2>Register here</h2>
+        </div>
+        <div class="info">
+          <input class="fname" type="text" name="nome" placeholder=" Nome">
+          <input class="fname" type="text" name="cognome" placeholder=" Cognome">
+          <input type="text" name="email" placeholder=" Email">
+          <select name="sesso" id="" required>
+            <option disabled selected value>Sesso</option>
+            <option value="maschio">Maschio</option>
+            <option value="femmina">Femmina</option>
+          </select>
+          <select name="anno" id="" required>
+                <option disabled selected value>Anno</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
                 <option value="4">4</option>
                 <option value="5">5</option>
-            </select>
-            Sezione:
-            <select name="sezione" id="" required>
-                <option disabled selected value> -- Seleziona un'opzione -- </option>
+          </select>
+          <select name="sezione" id="" required>
+                <option disabled selected value>Sezione</option>
                 <option value="A">A</option>
                 <option value="B">B</option>
                 <option value="C">C</option>
                 <option value="D">D</option>
             </select>
-            indirizzo:
             <select name="indirizzo" id="" required>
-                <option disabled selected value> -- Seleziona un'opzione -- </option>
+                <option disabled selected value>Indirizzo</option>
                 <option value="INF">INF</option>
                 <option value="LSA">LSA</option>
                 <option value="RIM">RIM</option>
                 <option value="MEC">MEC</option>
             </select>
-            
-        </p>
-        <p>Email:
-            <input type="email" name="email" id="email" required>
-        </p>
+            <input class="fname" type="text" name="numTel" id="numTel" placeholder=" Num Telefono (non obbligatorio)">
+        </div>
+        
+        <button type="submit" href="/">Registrati</button>
+        <button type="reset" href="register.php">Annulla</button>
+      </form>
+    </div>
+  </body>
         <!--<p>Password:
             <input type="password" name="password" id="password" required>
             <input type="checkbox" onclick='seePassword("password")'>Show Password
@@ -68,15 +72,7 @@ require_once '../Database/config.php';
             <input type="checkbox" onclick='seePassword("conf_password")'>Show Password
         </p>
         LA PASSWORD ALLA REGISTRAZIONE E' RANDOM-->
-        <p>Num Telefono:
-        <input type="text" name="numTel" placeholder="non obbligatorio" id="">
-        <p>
-            <input type="submit" value="Registrati"> o
-            <a href="register.php">Annulla</a>
-        </p>
     
-        
-    </form>
     <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     $nome = mysqli_real_escape_string($link, $_POST['nome']);
--- Sitoweb/profile/profile.php
+++ Sitoweb/profile/profile.php
@@ -10,86 +10,90 @@ if(!isset($_SESSION['uname'])){
 <html lang="it">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>HomePage</title>
     <style>
       body {
-    height: 100vh;
-    margin: 0;
-    background-color: rgba(0, 0, 0, 0.4);
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    background-position: center;
-    display: flex;
-    flex-flow:column;
-    justify-content: flex-start;
-    
-    
-  }
-  
-  .menu-bar {
-    border-radius: 25px;
-    height: -webkit-fit-content;
-    height: -moz-fit-content;
-    height: fit-content;
-    display: inline-flex;
-    background-color: rgba(0, 0, 0, 0.4);
-    -webkit-backdrop-filter: blur(10px);
-    backdrop-filter: blur(10px);
-    align-items: center;
-    padding: 0 50px;
-    margin: 20px 0 0 0;
-  }
-  .menu-bar li {
-    list-style: none;
-    color: white;
-    font-family: sans-serif;
-    font-weight: bold;
-    padding: 12px 16px;
-    margin: 0 8px;
-    position: relative;
-    cursor: pointer;
-    white-space: nowrap;
-  }
-  .menu-bar li::before {
-    content: " ";
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-    z-index: -1;
-    transition: 0.2s;
-    border-radius: 25px;
-  }
-  .menu-bar li:hover {
-    color: black;
-  }
-  .menu-bar li:hover::before {
-    background: linear-gradient(to bottom, #e8edec, #d2d1d3);
-    box-shadow: 0px 3px 20px 0px black;
-    transform: scale(1.2);
-  }
-  a {
-    color: white;
-    text-decoration: none; 
-    font-weight: bold;
-  }
-  a:hover {
-    color: black;
-  }
-  .logo{
-    width:100%;
-    height:40px;
-  }
-  .profile {
-  vertical-align: middle;
-  width: 3em;
-  height: 3em;
-  border-radius: 50%; 
+height: 100vh;
+margin: 0;
+background-color: rgba(0, 0, 0, 0.4);
+background-repeat: no-repeat;
+background-attachment: fixed;
+background-position: center;
+display: flex;
+flex-flow:column;
+justify-content: flex-start;
+
 
 }
+
+.menu-bar {
+border-radius: 25px;
+height: -webkit-fit-content;
+height: -moz-fit-content;
+height: fit-content;
+display: inline-flex;
+background-color: rgba(0, 0, 0, 0.4);
+-webkit-backdrop-filter: blur(10px);
+backdrop-filter: blur(10px);
+align-items: center;
+padding: 0 50px;
+margin: 20px 0 0 0;
+}
+.menu-bar li {
+list-style: none;
+color: white;
+font-family: sans-serif;
+font-weight: bold;
+padding: 12px 16px;
+margin: 0 8px;
+position: relative;
+cursor: pointer;
+white-space: nowrap;
+}
+.menu-bar li::before {
+content: " ";
+position: absolute;
+top: 0;
+left: 0;
+height: 100%;
+width: 100%;
+z-index: -1;
+transition: 0.2s;
+border-radius: 25px;
+}
+.menu-bar li:hover {
+color: black;
+}
+.menu-bar li:hover::before {
+background: linear-gradient(to bottom, #e8edec, #d2d1d3);
+box-shadow: 0px 3px 20px 0px black;
+transform: scale(1.2);
+}
+a {
+color: white;
+text-decoration: none; 
+font-weight: bold;
+}
+a:hover {
+color: black;
+}
+.logo{
+width:100%;
+height:40px;
+}
+.profilino {
+vertical-align: middle;
+width: 3em;
+height: 3em;
+border-radius: 50%; 
+}
+
     </style>
-    
-    <title>HomePage</title>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>   
 <ul class="menu-bar">
@@ -116,11 +120,16 @@ if(!isset($_SESSION['uname'])){
           <input type="search" name="cerca" id="cerca" placeholder="search">
         </li>
         <li>
-        <a href="../Profile/profile.php"><img src="../Immagini_profilo/rossi_mario.jpg" class="profile"></a>  <?php echo $_SESSION['uname'] ?>
+        <a href="../Profile/profile.php"><img src="../Immagini_profilo/rossi_mario.jpg" class="profilino"></a>  <?php echo $_SESSION['uname'] ?>
         </li>
+        
 </ul>
+  
+
+
       <!--il mio profilo-->
 <?php
+    
     require_once '../Database/config.php';
     $id=$_SESSION['id'];
     $sql="SELECT * FROM utente WHERE id='$id'";
@@ -131,7 +140,7 @@ if(!isset($_SESSION['uname'])){
             
             $result = mysqli_stmt_get_result($stmt);   
             $row = mysqli_fetch_array($result);
-              echo"<tr>";
+              /*echo"<tr>";
                   echo "<td>".$row['id']."</td>";
                   echo "<td>".$row['nome_utente']."</td>";
                   echo "<td>".$row['email']."</td>";
@@ -141,24 +150,144 @@ if(!isset($_SESSION['uname'])){
                   echo "<td>".$row['sezione']."</td>";
                   echo "<td>".$row['indirizzo']."</td>";
                   echo "<td>".$row['numTelefono']."</td>";
-                  echo "<td>".$row['sesso']."</td>";
-              echo"</tr>";
-              $idUtente=$row['id'];
-              $nomeUtente=$row['nome_utente'];
-              $emailUtente=$row['email'];
-              $passwordUtente=$row['password'];
-              $imgprofiloUtente=$row['immagine_profilo'];
-              $annoUtente=$row['anno'];
-              $sezioneUtente=$row['sezione'];
-              $indirizzoUtente=$row['indirizzo'];
-              $numtelefonoUtente=$row['numTelefono'];
-              $sessoUtente=$row['sesso'];
+                  echo "<td>".$row['sesso']."</td>";                  
+              echo"</tr>";*/
+              
             }
     }else{
         echo "something went wrong";
     }
+    
     mysqli_stmt_close($stmt);
 ?>  
-
+<div class="container emp-profile">
+            <form method="post">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="profile-img">
+                            <img src=<?php echo $row['immagine_profilo']?> alt=""/>
+                            <div class="file btn btn-lg btn-primary">
+                                Change Photo
+                                <input type="file" name="file"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="profile-head">
+                        <br>            
+                        <h5>
+                        <?php echo $row['nome_utente']?>
+                                    </h5>
+                                    <br><br><br>
+                            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
+                                </li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <input type="submit" class="profile-edit-btn" name="btnAddMore" value="Edit Profile"/>
+                    </div>
+                </div>
+                
+                    <div class="col-md-8">
+                        <div class="tab-content profile-tab" id="myTabContent">
+                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Username</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p><?php echo $row['nome_utente']?></p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Name</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Kshiti Ghelani</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Email</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p><?php echo $row['email']?></p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Classe</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p><?php echo $row['anno']. " ".$row['sezione']. " ".$row['indirizzo']?></p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Sesso</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p><?php echo $row['sesso']?></p>
+                                            </div>
+                                        </div>
+                            </div>
+                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Experience</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Expert</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Hourly Rate</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>10$/hr</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Total Projects</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>230</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>English Level</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Expert</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Availability</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>6 months</p>
+                                            </div>
+                                        </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <label>Your Bio</label><br/>
+                                        <p>Your detail description</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>           
+        </div>
     </body>
 </html>
